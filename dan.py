24dc60ddeefcba47fdefc687459f9d224c8533bc
Steve Newsted <newsted@gmail.com>
2019-12-13 19:39:35 -0500

    only white for now

--- dan.py
+++ dan.py
@@ -9,20 +9,20 @@ pygame.mouse.set_visible(False)
 
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 
-screen.fill((0,0,255))
-pygame.display.update()
-time.sleep(1)
-
-screen.fill((0,255,0))
-pygame.display.update()
-time.sleep(1)
-
-screen.fill((255,0,0))
-pygame.display.update()
-time.sleep(1)
-
+#screen.fill((0,0,255))
+#pygame.display.update()
+#time.sleep(1)
+#
+#screen.fill((0,255,0))
+#pygame.display.update()
+#time.sleep(1)
+#
+#screen.fill((255,0,0))
+#pygame.display.update()
+#time.sleep(1)
+#
 screen.fill((255,255,255))
 pygame.display.update()
-time.sleep(1)
+#time.sleep(1)
 
 pygame.quit()
